4d35aff4881567c29c95a29456f9786ecc7997c4
PierreBeur <cbeurtheret@ucsd.edu>
2022-11-10 08:41:36 -0800

    Part 2 Steps 6-8

--- sw.js
+++ sw.js
@@ -3,13 +3,21 @@
 
 const CACHE_NAME = 'lab-7-starter';
 
+const RECIPE_URLS = [
+  'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+  'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+  'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+  'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+];
+
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-      //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      // All of the URLs from RECIPE_URLs are added to the cache when the ServiceWorker is installed
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -21,20 +29,21 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  // Respond to the event by opening the cache using the name we gave above (CACHE_NAME)
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    return cache.match(event.request).then((cachedResponse) => {
+      // If the request is in the cache
+      if (cachedResponse) {
+        // Return with the cached version
+        return cachedResponse;
+      }
+      // Otherwise fetch the resource
+      return fetch(event.request).then((fetchedResponse) => {
+        // Add it to the cache
+        cache.put(event.request, fetchedResponse.clone());
+        // And return network response
+        return fetchedResponse;
+      });
+    });
+  }));
 });
